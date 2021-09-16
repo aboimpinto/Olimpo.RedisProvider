76db7e720cdffa43b0648e3e8b8f3dac9a9de26e
Paulo Aboim Pinto <aboimpinto@gmail.com>
2021-09-16 03:01:25 +0200

    Avalonia demo project added. launch and tasks files added

---
+++ Olimpo.RedisProvider.Avalonia.Demo/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace Olimpo.RedisProvider.Avalonia.Demo.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
